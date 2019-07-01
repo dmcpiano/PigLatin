474c12a6dbd15a7040c7acd9adb92afe6800e222
dervc <dmcpiano@umich.edu>
2019-07-01 08:43:42 -0400

    changes1

--- src/co/grandcircus/piglatin/PigLatin.java
+++ src/co/grandcircus/piglatin/PigLatin.java
@@ -1,5 +1,4 @@
 
-//stuck on consonant condition, will ask for help today
 
 package co.grandcircus.piglatin;
 
@@ -22,24 +21,28 @@ public class PigLatin {
 	
 	public static void translator (String userInput) {
 		
-			String[] vowels = {"a" ,"e" ,"i" ,"o" ,"u" };
-			String[] consonants = {"b" ,"c" ,"d" ,"f" ,"g" ,"h" ,"j","k" ,"l", "m", "n", 
-									"p", "q", "r", "s", "t", "v", "w", "x", "y", "z"}; 
+	
+		String[] vowels = {"a" ,"e" ,"i" ,"o" ,"u" };
+		String[] consonants = {"b" ,"c" ,"d" ,"f" ,"g" ,"h" ,"j","k" ,"l", "m", "n", 
+								"p", "q", "r", "s", "t", "v", "w", "x", "y", "z"}; 
+		
+		for (int i = 0; i  < userInput.length();i++) {
+		
+			char character = userInput.charAt(i);
 			
-			for (int i = 0; i  < userInput.length();i++) {
-			
-				char character = userInput.charAt(i);
-				
-				if (character=='a' || character=='e' || character=='i' || character == 'o' || character == 'u') {
-				String ans = userInput.substring(i) + userInput.substring( 0, i ) + "way";
-				System.out.println(ans);
-				return;
+			if (character=='a' || character=='e' || character=='i' || character == 'o' || character == 'u') {
+			String ans = userInput.substring(i) + userInput.substring( 0, i ) + "way";
+			System.out.println(ans);
+			return;
 				
 				
-				}		
+				}
+			}
+	
 	
 	}
- }
+	}
+ 
 	
-}
+
 
